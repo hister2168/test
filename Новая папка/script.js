b476c1db763bd3205bb048a7b2689115cbafdadd
дмитрий <whoami2168@gmail.com>
2020-04-07 02:42:39 +0300

    это наш первый коммит

---
+++ Новая папка/script.js
@@ -0,0 +1,29 @@
+'use script';
+
+// alert("HelloWorld");
+
+// let answer = confirm("Are you here?");
+// console.log(answer)
+
+
+// let answer = prompt("есть ли вам 18?","yes");
+
+//   console.log(typeof(answer));
+
+// инкременд/дикремент 
+let incr = 10,
+  decr = 10;
+console.log(++incr);
+console.log(--decr);
+
+console.log(5 % 2);
+console.log("2" == 2);
+console.log("2" === 2); //сравнение типов данных
+
+let isChecked = true,
+  isClose = false;
+
+  console.log(isChecked && isClose);//и
+  console.log(isChecked || isClose);//или
+  console.log(isChecked || !isClose);//отрицание
+   
---
+++ типы данных/script.js
@@ -0,0 +1,26 @@
+'use script';
+
+// я твоя подсказка
+// var leftBorderWidth = 1;
+// let second = 2;
+// const pi = 3.14;
+
+// console.log(second);
+var number = 5 ;
+var string = "hello";
+var sym = symbol();
+var boolean = true;
+null;
+undefined;
+var obj = {};
+let person = { 
+    name: "John",
+    age: 25,
+    isMarried: false
+};
+
+console.log(person["name"]);
+
+let arr = ['plum.png','orange.img','apple.jpg'];
+
+console.log(arr[1])
